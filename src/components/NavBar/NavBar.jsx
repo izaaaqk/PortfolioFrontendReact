64948adc2918a96dcd9caf69f70d28b2baad5472
Isaac Aguirre <isaac.aguirre@du.edu>
2022-09-07 21:29:38 -0600

    hello about

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
+import {HiMenuAlt4, HiX } from 'react-icons/hi';
+import { motion } from 'framer-motion';
 
-import { images } from "../../constants";
+import { images } from '../../constants';
 import './NavBar.scss';
 
 const NavBar = () => {
+    const [toggle, setToggle,] = useState(false);
+
     return (
         <nav className="app__navbar">
             <div className="app__navbar-logo">
@@ -18,6 +22,29 @@ const NavBar = () => {
                     </li>
                 ))}
             </ul>
+
+            <div className="app__navbar-menu">
+                <HiMenuAlt4 onClick={() => setToggle(true)}/>
+
+                {toggle && (
+                    <motion.div
+                        whileInView={{ x: [300, 0] }}
+                        transition={{durations: 0.85, ease: 'easeOut'}}
+                    >
+                        <HiX onClick={() => setToggle(false)} />
+                        <ul>
+                        {['home', 'about', 'contact', 'work', 'skills'].map((item) =>(
+                            <li  key={item}>
+                                <a href={`#${item}`} onClick={() => setToggle(false)}>{item} </a>
+
+                            </li>
+                        ))}
+                        </ul>
+
+                    </motion.div>
+                )}
+
+            </div>
         </nav>
     );
 };
